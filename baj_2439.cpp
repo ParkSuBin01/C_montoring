6325733b006fa7f28eaf200eaee6340c72b2ffce
ParkSubin01 <parksb0901@naver.com>
2020-09-17 11:48:03 +0900

    C_montoring github upload

---
+++ baj_2439.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i+1; j <= n; j++)
+		{
+			printf(" ");
+		}
+		for (int j = 1; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
